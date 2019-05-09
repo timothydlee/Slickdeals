9d486851b978dcfe09f511c7fc7eda240a3b2d68
Tim Lee <timothy.lee@intlfcstone.com>
2019-05-09 18:23:46 -0400

    Trying to get LBTATools to work.

--- Slickdeals/ViewController.swift
+++ Slickdeals/ViewController.swift
@@ -6,15 +6,4 @@
 //  Copyright © 2019 Timothy Lee. All rights reserved.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+import LBTATools
